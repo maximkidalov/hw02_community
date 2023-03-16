6772b18decbee8022d184a3cc9dc389770fe241a
maximkidalov <117586406+maximkidalov@users.noreply.github.com>
2023-03-16 19:27:37 +0300

    Delete yatube directory

--- yatube/yatube/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    # импорт правил из приложения posts
-    path('', include('posts.urls')),
-    path('admin/', admin.site.urls),
-] 
